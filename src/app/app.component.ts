b156b0bb83b990cfcc6f3e44bc17ab4582cc2c4d
Sahil Ahir <sahilahir333@gmail.com>
2025-02-22 02:00:14 -0600

    Phase 1 Part 2 Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
@@ -15,19 +16,30 @@ export class AppComponent {
   constructor(private router: Router) {}
 
   navigateHome(){
-    console.log("going to vehicles");
     this.router.navigate(['/home'])
   }
   navigateAbout(){
-    console.log("going to vehicles");
     this.router.navigate(['/about'])
   }
   navigateContact(){
-    console.log("going to vehicles");
     this.router.navigate(['/contact'])
   }
   navigateEvents(){
-    console.log("going to vehicles");
     this.router.navigate(['/events'])
+  }
+  navigateByLaws(){
+    this.router.navigate(['/by-laws'])
+  }
+  navigateResources(){
+    this.router.navigate(['/resources'])
+  }
+  navigateResourceMaterial(){
+    this.router.navigate(['/resource-material'])
+  }
+  navigateExecutiveBoard(){
+    this.router.navigate(['/executive-board'])
+  }
+  isActive(route: string): boolean {
+    return this.router.url === route;
   }
 }
